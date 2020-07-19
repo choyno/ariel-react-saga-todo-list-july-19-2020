c9a93737aa2551a7a41ed17ece28d38203e7c799
Ariel Lopez <ariel.lopezf@gmail.com>
2020-07-20 07:59:40 +0800

    mutate todo form

--- src/components/todoForm.js
+++ src/components/todoForm.js
@@ -2,18 +2,31 @@ import React, { useState } from 'react';
 
 const TodoForm = () => {
 
-  const [title, setTitle ] = useState();
-  const [description, setDescription ] = useState();
+  const [todo, setTodo ] = useState({title: "", description: ""});
+  const { title, description } = todo;
+
+  const handleTodoItem = (key,value) => {
+    setTodo({
+      ...todo,
+      [key]: value
+    });
+  }
 
   return (
     <>
     <h3> TODO FORM</h3>
     <label className="todo-label"> Title </label>
-    <input id="" name="title" value={title} style={{width: "100%"}}/>
+    <input id="" name="title"
+           value={title} style={{width: "100%"}}
+           onChange={(e) => handleTodoItem('title', e.target.value)}
+          />
     <br/>
     <br/>
     <label className="todo-label"> Description </label>
-    <textarea id="" name="description" value={title} style={{width: "100%"}}/>
+    <textarea id="" name="description"
+        value={description} style={{width: "100%"}}
+        onChange={(e) => handleTodoItem('description', e.target.value)}
+      />
     </>
   )
 }
